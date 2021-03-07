a4324810e3c95d2a7cb14216ee40651af0d047d3
krzysztof-grzybek <krzysztot.grzybek89@gmail.com>
2021-03-07 17:58:24 +0100

    make prediction

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
 
 iris = load_iris()
 X_train, X_test, y_train, y_test = train_test_split(iris['data'], iris['target'], random_state=0)
 
-ifx, ax = plt.subplots(3, 3, figsize=(15,15))
-plt.suptitle("irs_pairplot")
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(X_train, y_train)
 
-for i in range(3):
-    for j in range(3):
-        ax[i,j].scatter(X_train[:,j], X_train[:,i+1], c=y_train, s=60)
-        ax[i,j].set_xticks(())
-        ax[i,j].set_yticks(())
-        if i == 2:
-            ax[i,j].set_xlabel(iris['feature_names'][j])
-        if j== 0:
-            ax[i,j].set_ylabel(iris['feature_names'][i+1])
-        if j> i:
-            ax[i,j].set_visible(False)
-
-plt.show()
+X_new = np.array([[5, 2.9, 1 ,0.2]])
+prediciton = knn.predict(X_test)
+print(knn.score(X_test, y_test))
